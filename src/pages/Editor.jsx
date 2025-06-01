e94bb939b281ac9ee645b503d465dac485bf53b1
Shanukeshri <shanukeshri983@gmail.com>
2025-06-01 13:11:46 +0530

    added debouncing

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,16 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "../styles/Editor.css";
 
 import Sidebar from "./Sidebar";
 
 function Editor() {
+  const [text, setText] = useState("");
+  const id = useRef();
+  const paraRef = useRef(null);
+
+  const handler = (e) => {
+    setText(e.target.value);
+    if (id.current) {
+      clearTimeout(id.current);
+    }
+    id.current = setTimeout(() => {
+      console.log("stopped");
+      paraRef.current.textContent = "saving...";
+      setTimeout(() => {
+        paraRef.current.textContent = "";
+      }, 700);
+    }, 500);
+  };
 
   return (
     <>
       <Sidebar></Sidebar>
       <input className="title"></input>
-      <textarea className="textarea"></textarea>
-      <p className="status">status</p>
+      <textarea className="textarea" value={text} onChange={handler}></textarea>
+      <p className="status" ref={paraRef}></p>
     </>
   );
 }
