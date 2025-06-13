5576f409729c8e18c338544d07bdbca2028a17c9
Shanukeshri <shanukeshri983@gmail.com>
2025-06-14 02:09:55 +0530

    dynamic url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: function(origin, callback) {
         const allowedOrigins = [
-            'https://notes-frontend-rb43.onrender.com',
+            process.env.FRONTEND_URL,
             'http://localhost:5173'
         ];
 
